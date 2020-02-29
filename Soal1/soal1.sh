aa03f3a2ab8a0eabdbe46b92d81a8053663755a8
natihdevayanti <56763600+natihdevayanti@users.noreply.github.com>
2020-02-29 18:31:38 +0700

    Add files via upload

---
+++ Soal1/soal1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+echo "Soal 1: "
+a=$(awk -F"\t" 'NR > 1 {Data[$13] += $21} END { for (i in Data) print Data[i], i }' /Users/putrinatih/Downloads/Sample-Superstore.tsv | sort -g | head -1 | awk '{print $2}' )
+echo "$a"
+
+echo "Soal 2: "
+b=$(awk -F"\t" 'NR > 1 {if ($13 == "Central") {Reg[$11] += $21}} END { for (i in Reg) print Reg[i], i }' /Users/putrinatih/Downloads/Sample-Superstore.tsv | sort -g | head -2 | awk '{print $2}' )
+echo "$b"
+
+echo "Soal 3: "
+c=$(awk -F"\t" 'BEGIN { OFS=" "} {if ($13 == "Central") {if ($11 == "Texas" || $11 == "Illinois") {Data[$17] += $21} } } END { for (i in Data) print Data[i],i }' /Users/putrinatih/Downloads/Sample-Superstore.tsv | sort -g | head -10 | awk 'BEGIN {FS=" "} {print $2}' )
+echo "$c"
